5cf7e7954915856c8ad451896105229b1163e665
Kavya2228 <160152448+Kavya2228@users.noreply.github.com>
2024-02-27 20:08:13 +0530

    Commit Initial

---
+++ QRcode.py
@@ -0,0 +1,23 @@
+import qrcode
+import matplotlib.pyplot as plt
+
+url = "https://github.com/Jeevithayd/python-QRCode_Generator"
+
+# Generate QR code
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(url)
+qr.make(fit=True)
+
+# Create an image from the QR Code instance
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the image
+img.save("qr_code.png")
+
+print("QR code generated successfully for the provided URL.")
+plt.imshow(img, cmap='gray')
